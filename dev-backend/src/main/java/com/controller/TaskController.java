ef8abaf102f42e7b37a8fef85f27ef8a2688e0da
Odin-BN <odin.berumen@encora.com>
2025-01-24 08:08:02 -0700

    Controller for testing
    Controller for testing connection with frontend and new To Do button.

--- dev-backend/src/main/java/com/controller/TaskController.java
+++ dev-backend/src/main/java/com/controller/TaskController.java
@@ -1,15 +1,21 @@
 package com.odincode.TasksManagement.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
 //Receives the operations from the client
 
+import com.odincode.TasksManagement.model.TaskM;
 import com.odincode.TasksManagement.model.TaskModel;
 import com.odincode.TasksManagement.services.TaskServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-@RestController
+/*@RestController
 @RequestMapping("/name")
 public class TaskController {
     @Autowired
@@ -25,4 +31,21 @@ public class TaskController {
         return this.taskServices.saveTask(name, DueDate, flag, DoneDate, priority, CreateDate);
     }
 
+}*/
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8080")
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @PostMapping
+    public ResponseEntity<String> createTask(@RequestBody TaskM task){
+
+        System.out.println("Task received:");
+        System.out.println("Name: " + task.getName());
+        System.out.println("Priority: " + task.getPriority());
+        System.out.println("Deadline: " + task.getDeadline());
+
+        return ResponseEntity.ok("Task received and printed in console!");
+    }
 }

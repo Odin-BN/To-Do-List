0bbb47bb417d8784b0169a4589071ecdc3824db6
Odin-BN <odin.berumen@encora.com>
2025-01-27 06:13:44 -0700

    Model of the elements to display
    Model with the properties to display on the table of the frontend.

---
+++ dev-backend/src/main/java/com/model/TaskOUT.java
@@ -0,0 +1,73 @@
+package com.odincode.TasksManagement.model;
+
+import java.time.LocalDateTime;
+
+public class TaskOUT {
+    private Long id;
+    private String name;  //Limit to max 120 chars.
+    private String DueDate; //Optional
+    private boolean flag; //Done/Undone flag
+    private String priority; //High, Medium and Low.
+    private String rowColor;
+
+
+    public TaskOUT(Long id, String name, String DueDate, boolean flag, String priority, String rowColor){
+
+        this.name = name;
+        this.priority = priority;
+        this.DueDate = DueDate;
+        this.id = id;
+        this.flag = flag;
+        this.rowColor = rowColor;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDueDate() {
+        return DueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        DueDate = dueDate;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getRowColor() {
+        return rowColor;
+    }
+
+    public void setRowColor(String rowColor) {
+        this.rowColor = rowColor;
+    }
+
+}

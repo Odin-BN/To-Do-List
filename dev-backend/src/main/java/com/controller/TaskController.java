27c403cb76c7a8d9e573332ff9054e35e799aa91
Odin-BN <odin.berumen@encora.com>
2025-01-29 13:42:22 -0700

    Add files via upload

--- dev-backend/src/main/java/com/controller/TaskController.java
+++ dev-backend/src/main/java/com/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.odincode.TasksManagement.model.TaskModel;
 import com.odincode.TasksManagement.model.TaskOUT;
 import com.odincode.TasksManagement.services.TaskServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
@@ -36,6 +37,21 @@ public class TaskController {
     @PostMapping()
     public TaskModel saveTask(@RequestBody TaskAdd task){
         return this.taskServices.saveTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateTask(@PathVariable Long id, @RequestBody TaskAdd updatedTask) {
+        if (updatedTask == null || updatedTask.getDeadline().isEmpty()) {
+            updatedTask.setDeadline(null);
+        }
+        taskServices.updateTask(id, updatedTask);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskServices.deleteTask(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
